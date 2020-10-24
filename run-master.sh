aafb0db0a3aa4df2daabf191f19604287a905881
nik0chan <nik0chan>
2020-10-24 18:37:59 +0000

    Modificacion scripts .sh

--- run-master.sh
+++ run-master.sh
@@ -2,21 +2,20 @@ apt install -y git
 curl https://get.docker.com | sudo bash
 
 mkdir -p /srv/docker
-#	Configurar NFS
-echo 'maquina-1:/srv/nfs /srv/docker nfs defaults,nfsvers=3 0 0' >> /etc/fstab
-apt install -y nfs-kernel-server
-echo '/srv/nfs 10.132.0.0/24(rw,no_root_squash,no_subtree_check)' >> /etc/exports
-systemctl start nfs-kernel-server
-mount -a
+# Configurar NFS
+[[ -z "${NFS_SERVER}" ]]; echo '$NFS_SERVER:/srv/nfs /srv/docker nfs defaults,nfsvers=3 0 0' >> /etc/fstab
 
-docker swarm init 
+apt install -y nfs-kernel-server
+[[ -z "${NFS_CLIENTS}" ]]; echo '/srv/docker $NFS_CLIENTS(rw,no_root_squash,no_subtree_check)' >> /etc/exports
+echo systemctl start nfs-kernel-server
+echo mount -a
+
+docker swarm init
 docker swarm join-token manager|grep join  > /srv/docker/join.sh
 chmod +x !$
 
 cd /srv/docker
-git clone https://github.com/kpeiruza/swarm-cluster-example
+git clone https://github.com/nik0chan/cluster-swarm
 
 docker network create proxy -d overlay
 docker network create portainer_agent -d overlay
-
-
