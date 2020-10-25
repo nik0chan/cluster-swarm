f3cbfac7fc1e9624f676c3a4c90edd24d469e1ca
nik0chan <nik0chan>
2020-10-25 07:26:53 +0000

    correccion scripts run-master.sh

--- run-master.sh
+++ run-master.sh
@@ -1,12 +1,14 @@
+#!/bin/bash
+
 apt install -y git
 curl https://get.docker.com | sudo bash
 
 mkdir -p /srv/docker
 # Configurar NFS
-[[ -z "${NFS_SERVER}" ]]; echo '$NFS_SERVER:/srv/nfs /srv/docker nfs defaults,nfsvers=3 0 0' >> /etc/fstab
+[[ -z '${NFS_SERVER}' ]]; echo "$NFS_SERVER:/srv/nfs /srv/docker nfs defaults,nfsvers=3 0 0" >> /etc/fstab
 
 apt install -y nfs-kernel-server
-[[ -z "${NFS_CLIENTS}" ]]; echo '/srv/docker $NFS_CLIENTS(rw,no_root_squash,no_subtree_check)' >> /etc/exports
+[[ -z "${NFS_CLIENTS}" ]]; echo "/srv/docker $NFS_CLIENTS(rw,no_root_squash,no_subtree_check)" >> /etc/exports
 echo systemctl start nfs-kernel-server
 echo mount -a
 
