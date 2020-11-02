3df8172cd9550adbedcf7d64175d7a765f9c00e7
Nicolás Mantilla Simón <nicolasmantillasimon@gmail.com>
2020-11-02 19:02:26 +0100

    Adding Traefik 2.0 to monitor

--- deploy-services.sh
+++ deploy-services.sh
@@ -3,6 +3,12 @@
 PUBLIC_IP=$(curl ifconfig.co)
 sed -i -e "s/replaceme/$PUBLIC_IP/g" /srv/docker/monitor/stack.yaml
 sed -i -e "s/replaceme/$PUBLIC_IP/g" /srv/docker/portainer/stack.yaml
+sed -i -e "s/replaceme/$PUBLIC_IP/g" /srv/docker/traefik/stack.yaml
+
+mkdir -p /srv/docker/monitor/volumes/influxdb
+mkdir -p /srv/docker/monitor/volumes/grafana
+
+chmod 1777 /srv/docker/monitor/volumes/grafana
 
 docker network create --driver overlay proxy
 docker network create --driver overlay portainer_agent
